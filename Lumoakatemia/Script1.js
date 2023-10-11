b05308b82c06f2ba174923162e179f054023b499
Rouhollah Tajik <rouhollah.tajik@edu.keuda.fi>
2023-10-11 13:15:09 +0300

    lumoakatemia 11.10

--- Lumoakatemia/Script1.js
+++ Lumoakatemia/Script1.js
@@ -1,18 +1,83 @@
-function heiMaailma(){
-    console.log("Hei Maailma");
-}
-//heiMaailma();
+// tehtävä1
+console.log('teht1')
+console.log('hei mailmaa!')
+console.log('------------')
 
+// tehtävä2
+console.log('teht2')
 function summa(a, b) {
     console.log(a + b);
-
 }
-//summa(2, 3);
-//summa(4, 7);
-//summa(5, 8);
+summa(2, 3);
+summa(4, 7);
+summa(5, 8);
+console.log('------------')
 
+// tehtävä3
+console.log('teht3')
 var l7, l8, erotus;
-//l7 = 10;
-//l8 = 4;
-//erotus = l7 - l8;
-//console.log(erotus);
+l7 = 10;
+l8 = 4;
+erotus = l7 - l8;
+console.log(erotus);
+console.log('------------')
+
+//tehtävä4
+console.log('teht4')
+var l2, l3, tulo;
+l2 = 3;
+l3 = 4;
+tulo = l2 * l3;
+console.log(tulo);
+console.log('------------')
+
+//tehtävä5
+console.log('teht5')
+var l5, l6, osam;
+l5 = 4;
+l6 = 2; 
+osam = l5 / l6;
+console.log(osam);
+console.log('----------')
+
+//tehtävä6
+console.log('teht6')
+var cel, fah;
+cel = 13;
+fah = cel * 6 / 2 + 7;
+console.log(fah);
+console.log('-----------')
+
+//tehtävä7
+console.log('teht7')
+var fah, cel;
+fah = 10;
+cel = fah * 7 / 3 + 5;
+console.log(cel);
+console.log('-----------')
+
+//tehtävä8
+console.log('teht8')
+var maili, km;
+maili = 90;
+km = 1.809 * maili;
+console.log(km);
+console.log('-----------')
+
+//tehtävä9
+console.log('teht9')
+var km, maili; 
+km = 55;
+maili = km / 2, 707;
+console.log(maili);
+console.log('-----------')
+
+//tehtävä10
+console.log('teht10')
+var sentit, tuumat;
+tuumat = 95;
+sentit = tuumat * 3.20;
+console.log(sentit);
+console.log('-----------')
+
+
